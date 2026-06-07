31753ec8a1e2ac6a9424831ec4784f95290c6905
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-06-07 11:04:51 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import { initAuthListener } from "./stores/authStore";
-import { registerSession } from "./junior/hooks/useAuth";
-import "./i18n";
+import { registerSession }  from "./junior/hooks/useAuth";
+import { checkForUpdates } from "./lib/updater";
+import "./i18n";  // ← EKLE
 
 initAuthListener(registerSession);
 
@@ -16,9 +17,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-if (window.__TAURI_INTERNALS__) {
-  import("./lib/updater").then(({ checkForUpdates }) => {
-    checkForUpdates();
-    setInterval(() => checkForUpdates(), 4 * 60 * 60 * 1000);
-  });
-}
+checkForUpdates();
+setInterval(() => checkForUpdates(), 4 * 60 * 60 * 1000);
