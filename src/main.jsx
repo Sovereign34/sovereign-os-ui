ea576400620bace35f592eeb9818e18ee0e49a2c
Lionel <126454243+Sovereign34@users.noreply.github.com>
2026-06-21 10:32:30 +0300

    Refactor Router to use BrowserRouter only

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import { initAuthListener } from "./stores/authStore";
 import { registerSession }  from "./junior/hooks/useAuth";
@@ -9,14 +9,11 @@ import "./i18n";  // ← EKLE
 
 initAuthListener(registerSession);
 
-const isTauri = typeof window !== "undefined" && "__TAURI_INTERNALS__" in window;
-const Router = isTauri ? HashRouter : BrowserRouter;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <AppRouter />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
