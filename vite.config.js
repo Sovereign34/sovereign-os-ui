15a818fc1e12e75e876832e7bae835774e530909
Lionel <126454243+Sovereign34@users.noreply.github.com>
2026-06-21 09:52:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: process.env.TAURI_ENV_PLATFORM ? "./" : "/",
   clearScreen: false,
   server: {
     port: 5173,
